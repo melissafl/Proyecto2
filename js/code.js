a58dfc17630d1e08f682eab449dad40a472f41c5
Melissafl <melissa.fulopz@gmail.com>
2023-03-27 22:56:39 -0600

    localStorage

---
+++ js/code.js
@@ -0,0 +1,42 @@
+const nombre = document.getElementById("nombreIn");
+const apellido = document.getElementById("apellidoIn");
+const invitados = JSON.parse(localStorage.getItem("invitados")) || [];
+const cuerpoTabla = document.getElementById("cuerpoTabla");
+
+console.log(invitados);
+
+const agregarInvitado = () => {
+  const invitado = {
+    id: crypto.randomUUID(),
+    nombre: nombre.value,
+    apellido: apellido.value,
+  };
+
+  invitados.push(invitado);
+  localStorage.setItem("invitados", JSON.stringify(invitados));
+  mostrarInvitado();
+};
+
+const mostrarInvitado = () => {
+  cuerpoTabla.innerHTML = "";
+  invitados.forEach((invitado) => {
+    cuerpoTabla.innerHTML += `<tr>
+    <th scope="row">${invitado.id}</tr>
+    <td>${invitado.nombre}</td>
+    <td>${invitado.apellido}</td>
+    <td>
+    <button
+        type="button"
+        class="eliminarInvitado"
+        onclick="eliminarInvitado('${invitado.id}')"
+    >
+    Eliminar
+    </button><td>    
+    </tr>`;
+  });
+};
+
+const eliminarInvitado = (id) => {
+  console.log(id);
+  mostrarInvitado();
+};
